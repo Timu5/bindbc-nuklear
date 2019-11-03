fe564206c4185a39a37726736099be5c8afc5be0
Timu5 <timusziomek@gmail.com>
2019-11-03 13:18:48 +0100

    Remove nogc nothrow from nk_command_delegate and nk_draw_command_delegate

--- source/bindbc/nuklear/macros.d
+++ source/bindbc/nuklear/macros.d
@@ -25,29 +25,29 @@ version (NK_ALL)
     version = NK_UINT_DRAW_INDEX;
 }
 
-@nogc nothrow {
+alias nk_command_delegate = void delegate(const(nk_command)*);
+version(NK_INCLUDE_VERTEX_BUFFER_OUTPUT) {
+    alias nk_draw_command_delegate = void delegate(const(nk_draw_command)*);
+}
 
-    alias nk_command_delegate = void delegate(const(nk_command)*);
-    version(NK_INCLUDE_VERTEX_BUFFER_OUTPUT)
-    {
-        alias nk_draw_command_delegate = void delegate(const(nk_draw_command)*);
+pragma(inline, true) {
+    void nk_foreach(nk_context* ctx, nk_command_delegate block) {
+        for (auto c = nk__begin(ctx); c != null; c = nk__next(ctx, c)) {
+            block(c);
+        }
     }
 
-    pragma(inline, true) {
-        void nk_foreach(nk_context* ctx, nk_command_delegate block) {
-            for (auto c = nk__begin(ctx); c != null; c = nk__next(ctx, c)) {
+    version(NK_INCLUDE_VERTEX_BUFFER_OUTPUT) {
+        void nk_draw_foreach(nk_context *ctx, const(nk_buffer) *b, nk_draw_command_delegate block) {
+            for (auto c = nk__draw_begin(ctx, b); c != null; c = nk__draw_next(c, b, ctx)) {
                 block(c);
             }
         }
+    }
+}
 
-        version(NK_INCLUDE_VERTEX_BUFFER_OUTPUT) {		
-            void nk_draw_foreach(nk_context *ctx, const(nk_buffer) *b, nk_draw_command_delegate block) {
-                for (auto c = nk__draw_begin(ctx, b); c != null; c = nk__draw_next(c, b, ctx)) {
-                    block(c);
-                }
-            }
-        }
-
+@nogc nothrow {
+    pragma(inline, true) {
         auto nk_tree_push(size_t line = __LINE__)(nk_context *ctx, nk_tree_type type, const(char) *title, nk_collapse_states state) {
             return nk_tree_push_hashed(ctx, type, title, state, null, 0, line);
         }
@@ -57,17 +57,17 @@ version (NK_ALL)
         }
 
         auto nk_tree_image_push(size_t line = __LINE__)(nk_context *ctx, nk_tree_type type, nk_image img, const(char) *title, nk_collapse_states state) {
-            return nk_tree_image_push_hashed(ctx, type, img, title, state, null,0, line);
+            return nk_tree_image_push_hashed(ctx, type, img, title, state, null, 0, line);
         }
         auto nk_tree_image_push_id(nk_context *ctx, nk_tree_type type, nk_image img, const(char) *title, nk_collapse_states state, int id) {
             return nk_tree_image_push_hashed(ctx, type, img, title, state, null, 0, id);
         }
 
         auto nk_tree_element_push(size_t line = __LINE__)(nk_context *ctx, nk_tree_type type, const(char) *title, nk_collapse_states state, int* selected) {
-            return nk_tree_element_push_hashed(ctx, type, title, state, selected, null, 0,line);
+            return nk_tree_element_push_hashed(ctx, type, title, state, selected, null, 0, line);
         }
         auto nk_tree_element_push_id(nk_context *ctx, nk_tree_type type, const(char) *title, nk_collapse_states state, int* selected, int id) {
-            return nk_tree_element_push_hashed(ctx, type, title, state, selected, null, 0,id);
+            return nk_tree_element_push_hashed(ctx, type, title, state, selected, null, 0, id);
         }
 
         version(NK_INCLUDE_VERTEX_BUFFER_OUTPUT) {
